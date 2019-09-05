6a75f3f078fbce960cc96e358324b518d312b13f
Taha Bootwala <taha@scogo.in>
2019-09-05 15:36:59 +0530

    shouldComponentUpdate and higher order component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Auxilliary from '../hoc/Auxilliary'
 import classes from './Cockpit.module.css'
 
-const cockpit = (props) => {
-    const assignedClasses = []
-    let btnClass = ''
-    if (props.showPersons) {
-        btnClass = classes.Red
-    }
+const Cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect')
+  })
+  const assignedClasses = []
+  let btnClass = ''
+  if (props.showPersons) {
+      btnClass = classes.Red
+  }
 
-    if (props.persons.length <= 2) {
-      assignedClasses.push( classes.red )
-    }
-    if (props.persons.length <= 2) {
-      assignedClasses.push( classes.bold )
-    }    
-    return (
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working</p>
-            <button
-            className={btnClass}
-            onClick={props.click}>Toggle Persons</button>
-        </div>
-    )
+  if (props.persons.length <= 2) {
+    assignedClasses.push( classes.red )
+  }
+  if (props.persons.length <= 2) {
+    assignedClasses.push( classes.bold )
+  }    
+  return (
+    <Auxilliary>
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={assignedClasses.join(' ')}>This is really working</p>
+          <button
+          className={btnClass}
+          onClick={props.click}>Toggle Persons</button>
+      </div>
+      <h2>Hello</h2>
+    </Auxilliary>
+  )
 }
 
-export default cockpit
+export default Cockpit
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from './Person/Person'
 
 
-const persons = (props) => props.persons.map( (person, index) => {
-    return <Person 
-        click={() => props.click(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        change={(event) => props.change(event, person.id)}
-    />
-} )
+class Persons extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate')
+        console.log(nextProps, nextState)
+        return nextProps.persons !== this.props.persons
+    }
+    render() {
+        return this.props.persons.map( (person, index) => {
+            return <Person 
+                click={() => this.props.click(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                change={(event) => this.props.change(event, person.id)}
+            />
+        } )
+    }
+}
 
-export default persons
+export default Persons
