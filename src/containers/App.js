ad5356b199893d6011077f24be15e67e40dca5a0
Taha Bootwala <taha@scogo.in>
2019-09-09 12:37:17 +0530

    My Burger App Dynamic BurgerIngredients and skeleton of BuildControls

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Layout from '../components/Layout/Layout'
+import BurgerBuilder from './BurgerBuilder/BurgerBuilder'
 import classes from './App.module.css';
 
 class App extends Component {
   
   render() {
     return (
-      <div className={classes.App}>
-        Hello
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }  
