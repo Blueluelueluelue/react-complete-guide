2fa2baf52dbb0f9f522611283d1bd6b1571d7913
Taha Bootwala <taha@scogo.in>
2019-09-09 18:11:40 +0530

    My Burger app

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import classes from './DrawerToggle.module.css';
 
 const DrawerToggle = (props) => {
     return (
-        <div onClick={props.toggle}>MENU</div>
+        <div onClick={props.toggle} className={classes.DrawerToggle}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
     );
 }
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import classes from './Modal.module.css'
 
 import Backdrop from '../Backdrop/Backdrop'
 
-const Modal = (props) => {
-    return (
-        <Fragment>
-            <Backdrop show={props.show} click={props.modalClosed}/>
-            <div 
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </Fragment>            
-    );
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    UNSAFE_componentWillUpdate() {
+        console.log('[Modal.js] will update')
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} click={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Fragment>            
+        );
+    }
 }
 
 export default Modal
